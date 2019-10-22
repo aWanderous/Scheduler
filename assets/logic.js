f64e6ca3e05bcb5b92e8ea0e9d07a83c15735e73
Alex Wan <a_wanderous@hotmail.com>
2019-10-22 15:25:40 +1100

    tidy code

--- assets/logic.js
+++ assets/logic.js
@@ -1,4 +1,3 @@
-
 var firebaseConfig = {
     apiKey: "AIzaSyDQ3mdt1BcAlNPbhd1ZlPWsnVV5yctEaEc",
     authDomain: "click-counter-fa4ec.firebaseapp.com",
@@ -7,68 +6,66 @@ var firebaseConfig = {
     storageBucket: "click-counter-fa4ec.appspot.com",
     messagingSenderId: "558313563597",
     appId: "1:558313563597:web:944018767c68a5116c5084"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-
-
-$("button").click(function(event){
+$("button").click(function (event) {
     event.preventDefault();
-    
+
     // prevent empty values entered 
     if ($("#name").val() === "" ||
-    $("#place").val() === "" ||
-    $("#started").val() === "" ||
-    $("#rate").val() === "") {
-        
+        $("#place").val() === "" ||
+        $("#started").val() === "" ||
+        $("#rate").val() === "") {
+
         alert("Please fill in all details to add new ship");
-        
+
     } else {
-        
-    var name = $("#name").val();
-    var place = $("#place").val();
-    var started = $("#started").val();
-    var rate = $("#rate").val();
-    
-    database.ref().push({
-        name: name,
-        destination: place,
-        started: started,
-        frequenucy: rate
-    });
 
-    $("#newShips")[0].reset();
+        var name = $("#name").val();
+        var place = $("#place").val();
+        var started = $("#started").val();
+        var rate = $("#rate").val();
 
-}
+        database.ref().push({
+            name: name,
+            destination: place,
+            started: started,
+            frequenucy: rate
+        });
+
+        $("#newShips")[0].reset();
+
+    }
 });
 
 // time
-setInterval(function clock(){ 
-$("#time").html("Current time: " + moment().format("HH:mm:ss"))
+setInterval(function clock() {
+    $("#time").html("Current time: " + moment().format("HH:mm:ss"))
 }, 1000);
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     renderInfo();
 });
 
-function renderInfo () {
+function renderInfo() {
     $("#info").empty();
-    database.ref().on("child_added", function(snapshot){
+    database.ref().on("child_added", function (snapshot) {
         var newRow = $("<tr>")
-        .appendTo("tbody");
+            .appendTo("tbody");
         // ship name
         $("<td>").text(snapshot.val().name)
-        .appendTo(newRow);
+            .appendTo(newRow);
         // destination
         $("<td>").text(snapshot.val().destination)
-        .appendTo(newRow);
+            .appendTo(newRow);
         // frequenucy
         $("<td>").text(snapshot.val().frequenucy)
-        .appendTo(newRow);
+            .appendTo(newRow);
 
         var firstTimeConverted = moment(snapshot.val().started, "hh:mm").subtract(1, "years");
 
@@ -79,12 +76,12 @@ function renderInfo () {
 
         // next arrival time
         $("<td>").text(nextArrival)
-        .appendTo(newRow);
-        
+            .appendTo(newRow);
+
         // minutes away
         $("<td>").text(minutesAway)
-        .appendTo(newRow);
-    }, function(errorObject) {
+            .appendTo(newRow);
+    }, function (errorObject) {
         console.log("error with " + errorObject.code);
     });
 };
